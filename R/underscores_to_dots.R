f699b4042876da5cd2fe4a745debcdbf6625a098
Ron Keizer <ron@insight-rx.com>
2025-12-12 13:48:21 -0800

    misc updates

--- R/underscores_to_dots.R
+++ R/underscores_to_dots.R
@@ -1,7 +1,9 @@
 #' Convert underscores to dots
 #' 
+#' @export
 underscores_to_dots <- function(x) gsub("_", "\\.", x)
 
 #' Convert dots to underscores
 #' 
+#' @export
 dots_to_underscores <- function(x) gsub("\\.", "_", x)
